8432096799cdce879bd5dbc4984ab3f55cdf6459
gremble0 <hermanstornes@gmail.com>
2024-09-27 13:01:52 +0200

    Complete users section

--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,10 +1,29 @@
-function UsersSection() {
+import { useState, useEffect } from "react";
+
+export default function UsersSection() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://boolean-uk-api-server.fly.dev/hermagst/contact")
+      .then((res) => res.json())
+      .then((res) => setUsers(res))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          {users.map((user) => (
+            <li key={user.id} style={{ background: "#0d7f26" }}>
+              <img src={user.profileImage} alt={user.firstName} />
+              <h3>{user.firstName}</h3>
+              <p>Email: {user.email}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
-  )
+  );
 }
-
-export default UsersSection
