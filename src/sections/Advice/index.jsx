29197796b5977e357d4c9381f30be766328442be
gremble0 <hermanstornes@gmail.com>
2024-09-27 13:49:54 +0200

    Complete advice

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,41 @@
-function AdviceSection() {
+import { useState, useEffect } from "react";
+
+export default function AdviceSection() {
+  const [favorites, setFavorites] = useState([]);
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((res) => setAdvice(res.slip.advice))
+      .catch((error) => console.error("Error fetching advice:", error));
+  };
+
+  const saveToFavorites = () => {
+    setFavorites([...favorites, advice]);
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="adivce-slip">
+        <h3>Some Advice</h3>
+        <p>{advice}</p>
+        <button onClick={fetchAdvice}>Get More Advice</button>
+        <button onClick={saveToFavorites}>Save to Favourties</button>
+      </section>
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <ul>
+          {favorites.map((a, index) => (
+            <li key={index}>{a}</li>
+          ))}
+        </ul>
+      </section>
     </section>
-  )
+  );
 }
-
-export default AdviceSection
